8a6b9f4d64f5e6b4448d48a37fc476faa7857c29
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-17 20:58:05 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+# pontos = 0
+# questao = 1
+# while questao <= 3:
+#     resposta = input(f"Resposta da questão {questao}: ")
+#     if questao == 1 and resposta == "b":
+#         pontos = pontos + 1
+#     elif questao == 2 and resposta == "a":
+#         pontos = pontos + 1
+#     elif questao == 3 and resposta == "d":
+#         pontos = pontos + 1
+#     questao = questao + 1
+# print(f"O aluno fez {pontos} ponto(s).")
+
+# n = 1 #contador
+# soma = 0 #acumulador
+# while n <= 10:
+#     x = float(input(f"Digite o número {n}: "))
+#     soma = soma + x
+#     n = n + 1
+# print(f"Soma = {soma:.2f}")
+
+#EX 8
+
+# deposito = float(input(" Escreva o deposito inicial:"))
+# taxa = float (input(" Escreva a taxa de juros de uma poupança em porcentagem:"))
+# mes = 1
+# while mes <=24:
+#     deposito = deposito + ( deposito * (taxa/100))
+#     print(f" O saldo do mes {mes} é de {deposito:.2f} ")
+#     mes= mes + 1
+# print (f"o ganho total foi {deposito:.2f}" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
